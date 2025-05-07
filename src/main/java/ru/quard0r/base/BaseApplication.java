9016a5f2ef546c78bacb9addb2e7065b43de6314
Ilya <mrhide5010@gmail.com>
2025-05-07 11:58:14 +0300

    п

--- src/main/java/ru/quard0r/base/BaseApplication.java
+++ src/main/java/ru/quard0r/base/BaseApplication.java
@@ -2,8 +2,10 @@ package ru.quard0r.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class BaseApplication {
 
     public static void main(String[] args) {
